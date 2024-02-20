ac2712f2363abc2ecd79b41e2745a3ccc7eac557
Jim <jimorcheson@gmail.com>
2024-02-20 11:21:10 -0500

    Print devices found
    Delete incorrectly added device.go file.
    Add go-soapy-sdr package to project.

--- enumeratesdrs/enumeratesdrs.go
+++ enumeratesdrs/enumeratesdrs.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/pothosware/go-soapy-sdr/pkg/device"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	// List all devices
+	devices := device.Enumerate(nil)
+	for i, dev := range devices {
+		fmt.Printf("Found device #%v: ", i)
+		for k, v := range dev {
+			fmt.Printf("%v=%v, ", k, v)
+		}
+		fmt.Printf("\n")
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jimorc/gosdr
 
 go 1.22.0
+
+require github.com/pothosware/go-soapy-sdr v0.7.4
