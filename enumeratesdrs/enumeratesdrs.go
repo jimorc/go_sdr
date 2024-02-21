3d2182161fac86f23751512ae864847ec05dae0e
Jim <jimorcheson@gmail.com>
2024-02-21 06:55:18 -0500

    Adjust print of devices

--- enumeratesdrs/enumeratesdrs.go
+++ enumeratesdrs/enumeratesdrs.go
@@ -10,9 +10,9 @@ func main() {
 	// List all devices
 	devices := device.Enumerate(nil)
 	for i, dev := range devices {
-		fmt.Printf("Found device #%v: ", i)
+		fmt.Printf("Found device #%v:\n", i)
 		for k, v := range dev {
-			fmt.Printf("%v=%v, ", k, v)
+			fmt.Printf("%v=%v\n", k, v)
 		}
 		fmt.Printf("\n")
 	}
