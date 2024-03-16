96706770cef5b886917c15c48f8889c699b34c13
jim <jimorcheson@gmail.com>
2024-03-16 12:04:42 -0400

    Place soapy logging into separate module

---
+++ internal/soapy_logging/soapy_logging.go
@@ -0,0 +1,60 @@
+package soapy_logging
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/pothosware/go-soapy-sdr/pkg/sdrlogger"
+)
+
+var soapyLogfileName string
+
+// createSDRLogfileName sets the name of the sdr logging file and either creates or truncates the file
+func setSoapyLogfileName(name string) {
+	soapyLogfileName = name
+	logFile, err := os.Create(soapyLogfileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = logFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+// logSoapy receives and prints Soapy messages to be logged to the log file
+func LogSoapy(level sdrlogger.SDRLogLevel, message string) {
+	levelStr := "Unknown"
+	switch level {
+	case sdrlogger.Fatal:
+		levelStr = "Fatal"
+	case sdrlogger.Critical:
+		levelStr = "Critical"
+	case sdrlogger.Error:
+		levelStr = "Error"
+	case sdrlogger.Warning:
+		levelStr = "Warning"
+	case sdrlogger.Notice:
+		levelStr = "Notice"
+	case sdrlogger.Info:
+		levelStr = "Info"
+	case sdrlogger.Debug:
+		levelStr = "Debug"
+	case sdrlogger.Trace:
+		levelStr = "Trace"
+	case sdrlogger.SSI:
+		levelStr = "SSI"
+	}
+	logFile, err := os.OpenFile(soapyLogfileName, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer logFile.Close()
+
+	_, err = logFile.WriteString(fmt.Sprintf("Soapy Logged: [%v] %v\n", levelStr, message))
+	if err != nil {
+		log.Panic(err)
+	}
+}
