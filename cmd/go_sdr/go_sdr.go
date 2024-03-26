99942ae62be9c62ebfbfca8acf427d26991388a5
jim <jimorcheson@gmail.com>
2024-03-26 10:37:14 -0400

    Add TwoStateToolbarAction widget
    Add TwoStateToolbarAction.
    Add code to go_sdr.go to use it.

--- cmd/go_sdr/go_sdr.go
+++ cmd/go_sdr/go_sdr.go
@@ -1,8 +1,16 @@
 package main
 
-import (
-	"internal/soapy_logging"
+// go:generate fyne go_sdr -o bundled.go $GOPATH/go_sdr/cmd/go_sdr/images/start.svg
 
+import (
+	"fmt"
+	"internal/gui"
+	"internal/soapy_logging"
+	"log"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 	"github.com/pothosware/go-soapy-sdr/pkg/sdrlogger"
 )
 
@@ -11,4 +19,33 @@ func main() {
 	sdrlogger.RegisterLogHandler(soapy_logging.LogSoapy)
 	sdrlogger.SetLogLevel(sdrlogger.Info)
 	sdrlogger.Log(sdrlogger.Info, "go_sdr Logging")
+
+	sdrApp := app.New()
+	mainWindow := sdrApp.NewWindow("go_sdr")
+
+	start, err := fyne.LoadResourceFromPath("images/start.svg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stop, err := fyne.LoadResourceFromPath("images/stop.svg")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	startAction := gui.NewTwoStateToolbarAction(start, stop, startButtonTapped, stopButtonTapped)
+	toolBar := widget.NewToolbar(startAction)
+
+	mainWindow.SetContent(toolBar)
+
+	mainWindow.ShowAndRun()
+
+}
+
+func startButtonTapped() {
+	fmt.Println("In startButtonTapped")
+}
+
+func stopButtonTapped() {
+	fmt.Println("In stopButtonTapped")
 }
